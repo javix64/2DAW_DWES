b991b0fe11cd5f3bdfa1ff389748ea86f6dbd79d
javix64 <jav1.x64@gmail.com>
2020-10-23 13:02:48 +0200

    added exercise1-array

--- t2/arrays/exercise1.php
+++ t2/arrays/exercise1.php
@@ -1,1 +1,21 @@
 <?php
+/*
+Escribe un script que almacene un array de 8 números enteros:
+a.recorrerá el array y lo mostrará
+b.lo ordenará y lo mostrará
+c.mostrará su longitud
+d.buscará un elemento dentro del array
+e.buscará un elemento dentro del array, pero por el parámetro que llegue a la URL
+Para mostrar los elementos del array en cada uno de los apartados se creará una función llamada mostrarArray(). 
+*/
+$array1=[124,241,35446,13445,57531,635,71,908];
+echo "A ordenado <br>";
+
+foreach ($array1 as $key) {
+    echo "$key"."<br>";
+}
+
+echo "B. ordenarlo: <br>";
+
+$array_ordenado=sort($array1);
+echo $array_ordenado;
